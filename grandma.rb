95678ad0bee441379cbfcba77e44999ecfb81bce
Jacob Loris <jacobloris@outlook.com>
2021-05-19 00:23:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma
   if string == "I LOVE YOU GRANDMA!"
-    return == "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
   return "HUH? SPEAK UP, SONNY!"
   elsif string == string.upcase
